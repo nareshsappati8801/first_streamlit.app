2cc6ed338a8448d52664516399df51d0b25e43d4
nareshsappati8801 <132116471+nareshsappati8801@users.noreply.github.com>
2023-04-29 08:18:51 +0530

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,5 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.text('Build Your Own Fruit Smoothie')
 
-my_fuits_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
