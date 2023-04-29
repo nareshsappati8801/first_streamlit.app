5f4463ac2c87b6ee76bf4ca79fefd51fc0df11f4
nareshsappati8801 <132116471+nareshsappati8801@users.noreply.github.com>
2023-04-29 08:22:38 +0530

    Update streamlit_app.py
    beautification

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,12 @@ import pandas as pd
 
 streamlit.title('My Parents new healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-streamlit.text('Build Your Own Fruit Smoothie')
+streamlit.header('Breakfast Favourites')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑 Avacado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
